3f6cbefd72cd4f870e7f527dd16a147a98a1801a
Rocio del Pilar Chamorro Ramos <rocio_chamorro@outlook.com>
2021-09-07 14:11:49 -0500

    welcome view improvements

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,10 +1,9 @@
-<!-- Please remove this file from your project -->
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <HausLogo size="hausLogo-2xl"/>
+    <div class="max-w-4xl mx-auto px-6 lg:px-8">
+      <HausLogo size="lg:hausLogo-2xl"/>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <h1 class="text-2xl leading-7 font-semibold">
+        <h1 class="sm:text-2xl leading-7 font-semibold">
           ¡Bienvenido! Compra tu vivienda nueva
         </h1>
       </div>
